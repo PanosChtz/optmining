055d5cba10431ceb498ec89a5bf1a0def923f466
Panos Chatzigiannis <bomberb17@gmail.com>
2019-06-07 19:08:16 -0400

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,12 +8,12 @@ export default class Home extends Component {
       <h2>About</h2>
       <p>Smart Miner is a web application that allows users to find the most optimized distribution of their hash power. The application uses live
       currency, pool, and user configurated data to compute the most optimial distribution.</p>
-      <h4><a href="https://github.com/PanosChtz/Mining-pools/blob/master/Diversification%20across%20pools.pdf">White Paper</a></h4>
+      <h4><a href="https://arxiv.org/abs/1905.04624v2">Paper</a></h4>
       <br/>
       <h2>Team</h2>
-      <h4>Foteini Baldimtsi (<a href="https://cs.gmu.edu/directory/detail/65/">GMU</a>)</h4>
-      <h4>Panos Chatzigiannis (<a href="https://mason.gmu.edu/~pchatzig/">GMU</a>)</h4>
-      <h4>Jiasun Li (<a href="http://business.gmu.edu/facultyandresearch/faculty/finance/profile/48/112/">GMU</a>)</h4>
+      <h4><a href="https://cs.gmu.edu/~pchatzig/">Panos Chatzigiannis</a></h4>
+      <h4><a href="http://www.baldimtsi.com/">Foteini Baldimtsi</a></h4>
+      <h4><a href="https://sites.google.com/site/jiasunlihome/">Jiasun Li</a></h4>
       </div>
     );
   }
